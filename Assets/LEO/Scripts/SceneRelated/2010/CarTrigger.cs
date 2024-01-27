02ec4e1a7517023b0fbd79c11be88c264e7c741d
latexgrime <salestigarribia@gmail.com>
2024-01-27 19:17:23 +0400

    2010s

--- Assets/LEO/Scripts/SceneRelated/2010/CarTrigger.cs
+++ Assets/LEO/Scripts/SceneRelated/2010/CarTrigger.cs
@@ -4,27 +4,15 @@ using UnityEngine;
 
 public class CarTrigger : MonoBehaviour
 {
-    [SerializeField] LvlMan2010 LvlMan2010;             //  Reference to the level manager script
-    [SerializeField] Animator CarAnimator;              //  Reference to the car animator, to activate the car animation OnTriggerEnter
-    [SerializeField] Animator TrafficLightAnimator;     // Reference to the traffic light animator to turn it red
-    
-
-    // Start is called before the first frame update
-    void Start()
-    {
-                
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] Animator carAnim;
+    int iCount = 0;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        CarAnimator.SetBool("IsPassing", true);
-        //TrafficLightAnimator.
-        //LvlMan2010.ActivateCarSequence();
+        if (collision.gameObject.CompareTag("Player") && iCount<1) 
+        {
+            carAnim.SetBool("isPassing", true);
+            iCount++;
+        }
     }
 }
